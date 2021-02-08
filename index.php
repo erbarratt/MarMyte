e71fd3ce83e0a4b7d8e516b08c48fc4bd9fec0bd
Elliott Barratt <elbazuk@gmail.com>
2021-02-08 13:55:18 +0000

    Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,24 @@
+<?php
 
+/*
+* Surround whole app with error detection
+*/
+
+try{
+
+	/**
+	* call up the config
+	*/
+		if (file_exists(__DIR__.'/conf/config.php')) {
+			require(__DIR__.'/conf/config.php');
+		} else {
+			throw new \Exception('Config file not found.');
+		}
+	
+} catch (\Exception $e){
+	
+	echo $e->getMessage().'<br/>';
+	echo preg_replace('/\n/', '<br/>',$e->getTraceAsString());
+	exit;
+	
+}
