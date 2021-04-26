a0278b45ae6f37cf0416a0625c04ce9792720329
Elliott Barratt <elbazuk@gmail.com>
2021-04-26 14:41:44 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,24 +1,33 @@
 <?php
 
-/*
-* Surround whole app with error detection
-*/
-
-try{
-
-	/**
-	* call up the config
-	*/
-		if (file_exists(__DIR__.'/conf/config.php')) {
-			require(__DIR__.'/conf/config.php');
-		} else {
-			throw new \Exception('Config file not found.');
+//Surround whole app with error detection
+	try{
+	
+		//Call up the config
+			if (file_exists(__DIR__.'/conf/config.php')) {
+				require(__DIR__.'/conf/config.php');
+			} else {
+				throw new \Exception('Config file not found.');
+			}
+		
+	} catch (\Exception $e){
+	
+		$prgError = $e->getMessage();
+		$prgError .= PHP_EOL;
+		$prgError .= $e->getTraceAsString();
+		
+	} finally {
+	
+		if(isset($prgError)){
+		
+			error_log($prgError);
+		
+			if(!defined('DEBUG')){ define('DEBUG', true); }
+		
+			if(DEBUG){
+				echo preg_replace('/\n/', '<br/>',$prgError);
+			}
+		
 		}
 	
-} catch (\Exception $e){
-	
-	echo $e->getMessage().'<br/>';
-	echo preg_replace('/\n/', '<br/>',$e->getTraceAsString());
-	exit;
-	
-}
+	}
